fa2ea6e08020bfac25add065620eed5d738f58ba
Cosminflv <cosmin_popovici2012@yahoo.com>
2023-11-26 16:33:40 +0200

    Added request in client [ERROR]

--- Client/Client/PlayerWidget.cpp
+++ Client/Client/PlayerWidget.cpp
@@ -1,5 +1,13 @@
 #include "PlayerWidget.h"
 
+#include <sstream>
+#include <regex>
+#include <string>
+
+#include <cpr/cpr.h>
+#include <crow.h>
+
+
 PlayerWidget::PlayerWidget(QWidget* parent)
 {
 	setUpPlayerUI();
@@ -13,6 +21,11 @@ void PlayerWidget::setUpPlayerUI()
 {
 	QVBoxLayout* layout = new QVBoxLayout(this);
 	m_playerList = new QTextEdit(this);
+	
+	cpr::Response response = cpr::Get(cpr::Url{ "http://localhost:18080/playerinfo" });
+
+	auto players = crow::json::load(response.text);
+
 	m_playerList->setReadOnly(true);
 	layout->addWidget(m_playerList);
 }
