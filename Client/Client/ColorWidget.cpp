6f3e89e28064adf79f49c7c405f13d44ea276c1f
Ioana Manghiuc <ioana.manghiuc@student.unitbv.ro>
2023-11-28 00:18:37 +0200

    Constructor ColorWidget.

--- Client/Client/ColorWidget.cpp
+++ Client/Client/ColorWidget.cpp
@@ -1,1 +1,27 @@
 #include "ColorWidget.h"
+
+ColorWidget::ColorWidget(QWidget* parent)
+{
+    QGridLayout* layout = new QGridLayout(this);
+
+    int row = 0;
+    int col = 0;
+    for (const QString& colorCode : m_colors)
+    {
+        QColor color(colorCode);
+        QPushButton* colorButton = new QPushButton(this);
+        colorButton->setFixedSize(30, 30);
+        colorButton->setStyleSheet(QString("background-color: %1").arg(color.name()));
+        connect(colorButton, &QPushButton::clicked, this, [this, color]()
+            {
+                emit selectColor(color);
+            });
+
+        layout->addWidget(colorButton, row, col);
+        col++;
+        if (col >= 7) {
+            col = 0;
+            row++;
+        }
+    }
+}
