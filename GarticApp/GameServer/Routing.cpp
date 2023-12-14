96f5bfa07c3e4eda19279f5b0bba56c266305499
Cosminflv <cosmin_popovici2012@yahoo.com>
2023-12-14 20:37:18 +0200

    Added route for chat in Routing class

--- GarticApp/GameServer/Routing.cpp
+++ GarticApp/GameServer/Routing.cpp
@@ -1,4 +1,5 @@
 #include "Routing.h"
+#include "Chat.h"
 void Routing::Run(GameStorage& storage)
 {
 	CROW_ROUTE(m_app, "/")([]() {
@@ -48,6 +49,49 @@ void Routing::Run(GameStorage& storage)
 		
 					return crow::json::wvalue{ wordsJson };
 				});
+
+
+		Chat chat;
+        chat.WriteMessage({ "Server", "Hello darkness, my old friend!" });
+			CROW_ROUTE(m_app, "/chat")([&chat]()
+		{
+			std::vector<crow::json::wvalue> chatMessagesJson;
+			auto chatMessages = chat.getChat();
+			for (const auto& message : chatMessages)
+			{
+				crow::json::wvalue m
+				{
+					{"Username", message.first},
+					{"Message", message.second},
+				};
+
+				chatMessagesJson.push_back(m);
+			}
+			return crow::json::wvalue{ chatMessagesJson };
+		});
+
+
+			//405 Method Not Allowed
+	//CROW_ROUTE(m_app, "/receive_message")
+	//	.methods("POST"_method)
+	//	([&chat](const crow::request& req) {
+	//	// Access the transmitted JSON data from the client
+	//	auto json_data = crow::json::load(req.body);
+	//	if (!json_data) {
+	//		return crow::response(400);
+	//	}
+
+	//	if (json_data.has("username") && json_data.has("message")) {
+	//		std::string username = json_data["username"].s();
+	//		std::string password = json_data["message"].s();
+
+	//		chat.WriteMessage({ username, password });
+	//	}
+	//	else {
+	//		return crow::response(400);
+	//	}
+	//		});
+
 
 	m_app.port(18080).multithreaded().run();
 }
